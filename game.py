d871e761bc475457e01c084cc8ecc6d14ced00d5
Thato <148327843+ThatoTee9904@users.noreply.github.com>
2023-10-18 14:07:37 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,68 @@
+import random
+import time
+
+secret_number = random.randint(1,100)
+
+attempts = 0
+maximum attempts = 2
+score = 0
+leaderboard = []
+
+#Create a time function
+
+def timer(start_time)
+
+#Display Menu
+
+print("User menu")
+print("Pick number")
+print("Settings")
+print("Profile")
+print("Exit")
+
+print("2. Profile")
+
+
+if condition:
+  refiloe = 37
+  if refiloe < 37:
+    print("Iam young")
+  else:
+    print("Iam aged")
+
+
+
+if guess=secret_number:
+print("congrats you've won")
+
+# [Define a function to calculate score]
+
+def calculate_score (attempts):
+  return 10* attempts
+  if attemots = 2:
+  return calculate_score -5
+else:
+return calculate_score +10
+
+
+a=1
+b=2
+def add_numbers(a+b)
+result= a+b
+return result
+
+
+
+
+
+
+
+
+
+#Problem Statements
+#1. Imagine you are working on a project with a team, and you need to be able to keep #track of all tasks members do. How will you simplify this collaboration?
+
+#2. An educator wants to engage learners of an ECD center for special needs learners and teach them in a way that is entertaining and fun.
+#What programme can she use for this?
+
+#3. Picture yourself as a librarian managing a busy library. What program can you use to to catalog books, track borrowing and provide an easy way for all library members to access resources?
